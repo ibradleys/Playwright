a2649fa6a896c0526b346a345d04b2e0ba74f8a4
Bradley Santora <53438007+ibradleys@users.noreply.github.com>
2026-09-12 00:34:40 -0500

    Add files via upload

---
+++ playwright.config.js
@@ -0,0 +1,22 @@
+const { defineConfig } = require('@playwright/test');
+
+module.exports = defineConfig({
+
+    testDir: './tests',
+
+    timeout: 30000,
+
+    use: {
+
+        trace: 'on',
+
+        screenshot: 'only-on-failure',
+
+        video: 'retain-on-failure'
+    },
+
+    reporter: [
+        ['list'],
+        ['html', { open: 'never' }]
+    ]
+});
